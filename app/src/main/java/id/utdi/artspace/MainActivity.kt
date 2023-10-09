76e9af521a334484f6ef3008eedc73d5c58a98de
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-09 21:24:52 +0700

    artsapce

---
+++ app/src/main/java/id/utdi/artspace/MainActivity.kt
@@ -0,0 +1,157 @@
+package id.utdi.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ArtSpaceApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceApp() {
+    val artPieces = remember { generateArtPieces() }
+    var selectedArtPiece by remember { mutableStateOf(artPieces[0]) }
+    var currentIndex by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ArtPieceWithButtonAndImage(selectedArtPiece)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {
+                    currentIndex = (currentIndex - 1 + artPieces.size) % artPieces.size
+                    selectedArtPiece = artPieces[currentIndex]
+                },
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Text(text = "Previous", fontSize = 15.sp)
+            }
+
+            Button(
+                onClick = {
+                    currentIndex = (currentIndex + 1) % artPieces.size
+                    selectedArtPiece = artPieces[currentIndex]
+                },
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Text(text = "Next", fontSize = 15.sp)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        ArtPieceList(artPieces)
+    }
+}
+
+@Composable
+fun ArtPieceWithButtonAndImage(artPiece: ArtPiece) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = artPiece.imageResId),
+            contentDescription = artPiece.name,
+            modifier = Modifier
+                .size(200.dp)
+                .padding(16.dp)
+        )
+
+        Text(
+            text = artPiece.name,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Text(
+            text = artPiece.description,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Tambahkan aksi ketika tombol diklik */ },
+        ) {
+            Text(text = "Pilih antara Next atau Previous", fontSize = 24.sp)
+        }
+    }
+}
+
+@Composable
+fun ArtPieceList(artPieces: List<ArtPiece>) {
+    LazyColumn {
+        items(artPieces) { artPiece ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                // Tambahkan ikon di sini
+                Image(
+                    painter = painterResource(id = artPiece.imageResId),
+                    contentDescription = artPiece.name,
+                    modifier = Modifier.size(40.dp)
+                )
+
+                // Tambahkan teks nama art piece
+                Text(text = artPiece.name)
+
+                // Tambahkan teks deskripsi art piece
+                Text(text = artPiece.description)
+
+                // Tambahkan aksi ketika item diklik
+                // ...
+            }
+        }
+    }
+}
+
+data class ArtPiece(val id: Int, val name: String, val description: String, val imageResId: Int)
+
+fun generateArtPieces(): List<ArtPiece> {
+    return listOf(
+        ArtPiece(1, "Danau", "Pemandangan danau", R.drawable.my_icon1),
+        ArtPiece(2, "Gunung", "Pemandangan gunung", R.drawable.my_icon2),
+        // Tambahkan art pieces lainnya di sini
+    )
+}
+
